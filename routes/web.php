28333191be9f1cdcc80a3268e1c153f03f8514ee
ErrolKitsiiri <errolantonio325@gmail.com>
2024-09-30 15:02:19 +0300

    Reversed changes

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get();
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
